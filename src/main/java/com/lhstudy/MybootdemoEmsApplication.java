cac378981d1d1788b47b5ecf5843d4e6be8bf9f9
luoH0723 <3035419253@qq.com>
2020-05-18 14:46:02 +0800

    add some config and sql

--- src/main/java/com/lhstudy/MybootdemoEmsApplication.java
+++ src/main/java/com/lhstudy/MybootdemoEmsApplication.java
@@ -1,9 +1,11 @@
 package com.lhstudy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lhstudy.dao")
 public class MybootdemoEmsApplication {
 
     public static void main(String[] args) {
