7b6ba513aea5353b2f9e363726d045ec464abbd7
luoH0723 <3035419253@qq.com>
2020-05-18 13:10:57 +0800

    my first push

---
+++ src/main/java/com/lhstudy/controller/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.lhstudy.controller;
+
+//this class is use for test git
+public class HelloWorld {
+    public String sayHello(){
+        return "hello,world";
+    }
+}
---
+++ src/test/java/com/lhstudy/MybootdemoEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybootdemoEmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
